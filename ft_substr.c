adea91d8c354a6a1edd821f4c1cade219f782e9a
NLaw <nicklaw@gmail.com>
2022-09-21 11:36:09 +0700

    18

--- ft_substr.c
+++ ft_substr.c
@@ -10,8 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 char			*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    
+    size_t j;
+    int k;
+    char* ptr;
+    //allocates with malloc
+    ptr = malloc(len*sizeof(char));
+    //NULL if allocation fails
+    if (*ptr == '\0')
+    {
+        return('\0');
+    }
+    //copying to pointer using start as the index and stopping by the end of s or maximum len
+    for ( k = (start - 1), j = 0; j < len || !s[k]; k++, j++)       
+    {
+        ptr[j] = s[k];
+    }
+     //*check NULL terminate substring?
+     return(ptr);
 }
