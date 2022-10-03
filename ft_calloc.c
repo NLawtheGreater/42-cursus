6229efb3569f070e2d9048b752afe234d38faed1
niclaw <nicklaw@gmail.com>
2022-10-03 20:30:04 +0700

    77

--- ft_calloc.c
+++ ft_calloc.c
@@ -12,17 +12,18 @@
 #include "libft.h"
 #include <stdio.h>
 
+/*1.using malloc to allocate memory
+**2.check memory allocation, returns NULL if fail
+**3.using bzero to make zero*/
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (ptr == NULL)
 		return (NULL);
 	ft_bzero(ptr, (count * size));
 	return (ptr);
 }
-//check memory allocation, returns NULL if fail
-//using malloc to allocate memory
-//using bzero to make zero
-//return void pointer
